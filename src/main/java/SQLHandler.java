2cf999654c933e46ffbf73156a36f4069cfebec3
Xaria Davis <xariadavis@knights.ucf.edu>
2022-07-04 01:12:39 -0400

    implemented operations log req

--- src/main/java/SQLHandler.java
+++ src/main/java/SQLHandler.java
@@ -1,9 +1,15 @@
 import com.google.common.collect.Lists;
+import com.mysql.cj.jdbc.MysqlDataSource;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Properties;
 
 public class SQLHandler {
     private String errorString;
@@ -14,6 +20,12 @@ public class SQLHandler {
     private List<List<String>> rowData;
     private int rowsAffectedUpdate;
 
+    // operations log
+    private int numQueries = 0;
+    private int numUpdates = 0;
+    private MysqlDataSource operationDataSource;
+    private Connection operationsConnection;
+
     // connect to the database
     public void connectToDatabase() {
         try {
@@ -21,7 +33,11 @@ public class SQLHandler {
             System.out.println("Driver loaded");
             System.out.println("SUCCESSFUL CONNECTION");
 
-        } catch (ClassNotFoundException e) {
+            // connect to operations log database
+            readOperationProperties();
+            setOperationsConnection(getOperationDataSource().getConnection());
+
+        } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Cannot issue statements that do not produce result sets.");
             System.out.println("Issue in connectToDatabase");
             e.printStackTrace();
@@ -33,11 +49,21 @@ public class SQLHandler {
             Statement statement = connection.createStatement();
             ResultSet resultSet = null;
 
+            Statement operationsStatement = getOperationsConnection().createStatement();
+
             // if we have to use DML -- update, insert, delete then we need to use executeUpdate
             if(query.toLowerCase(Locale.ROOT).contains("select")) {
                 resultSet = statement.executeQuery(query);
+                setNumQueries(getNumQueries() + 1);
+                operationsStatement.executeUpdate("update operationscount set num_queries = " + getNumQueries());
             } else {
                 setRowsAffectedUpdate(statement.executeUpdate(query)); // if 0 nothing updated
+
+                // update numUpdates in operations log
+                setNumUpdates(getNumUpdates() + 1);     // increment count
+                // perform execute Update operation
+                operationsStatement.executeUpdate("update operationscount set num_updates = " + getNumUpdates());
+
             }
 
             ResultSetMetaData metaData;
@@ -72,6 +98,22 @@ public class SQLHandler {
             System.out.println("Issue in executeCommand");
             e.printStackTrace();
             setErrorString(e.getMessage());
+        }
+    }
+
+    private void readOperationProperties() {
+        InputStream inputStream;
+        Properties properties = new Properties();
+
+        try {
+            inputStream = new FileInputStream("properties/operationslog.properties");
+            properties.load(inputStream);
+            setOperationDataSource(new MysqlDataSource());
+            getOperationDataSource().setURL(properties.getProperty("MYSQL_DB_URL"));
+            getOperationDataSource().setUser(properties.getProperty("MYSQL_DB_USERNAME"));
+            getOperationDataSource().setPassword(properties.getProperty("MYSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -131,5 +173,37 @@ public class SQLHandler {
 
     public void setRowsAffectedUpdate(int rowsAffectedUpdate) {
         this.rowsAffectedUpdate = rowsAffectedUpdate;
+    }
+
+    public int getNumQueries() {
+        return numQueries;
+    }
+
+    public void setNumQueries(int numQueries) {
+        this.numQueries = numQueries;
+    }
+
+    public int getNumUpdates() {
+        return numUpdates;
+    }
+
+    public void setNumUpdates(int numUpdates) {
+        this.numUpdates = numUpdates;
+    }
+
+    public MysqlDataSource getOperationDataSource() {
+        return operationDataSource;
+    }
+
+    public void setOperationDataSource(MysqlDataSource operationDataSource) {
+        this.operationDataSource = operationDataSource;
+    }
+
+    public Connection getOperationsConnection() {
+        return operationsConnection;
+    }
+
+    public void setOperationsConnection(Connection operationsConnection) {
+        this.operationsConnection = operationsConnection;
     }
 }
